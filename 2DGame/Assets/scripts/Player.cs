6bc270970cfc19f12a3f5435e201743aa2b1ae98
fa10106618 <71679881+fa10106618@users.noreply.github.com>
2020-11-03 16:03:09 +0800

    2020/11/03碰撞

---
+++ 2DGame/Assets/scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門惹");
+        final.SetActive(true);
+
+    }
+
+}
