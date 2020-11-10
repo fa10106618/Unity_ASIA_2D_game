4879a26f6891b72fd869d0b225fa8c151bdffcc4
fa10106618 <71679881+fa10106618@users.noreply.github.com>
2020-11-10 16:12:23 +0800

    20201110

---
+++ 2DGame/Assets/scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void NextLevevl(string nameLv)
+    {
+        SceneManager.LoadScene(nameLv);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
--- 2DGame/Assets/scripts/Player.cs
+++ 2DGame/Assets/scripts/Player.cs
@@ -1,15 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
 
+    public Text textCount;
+
+    public int count;
+
     private void OnTriggerEnter2D(Collider2D collision)
+       
     {
-        print("碰到傳送門惹");
-        final.SetActive(true);
+        if(collision.name=="傳送門")
+        {
+            final.SetActive(true);
+        }
+
+
+        //print("碰到傳送門惹");
+        if (collision.tag == "鑽石")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+
+            textCount.text = "鑽石數量:" + count;
+        }
 
     }
 
